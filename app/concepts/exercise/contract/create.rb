b8008d49e39e349a22dc72b0e31977d3d1300a64
StanisLove Sid <joingamings@gmail.com>
2019-06-04 02:27:43 +0300

    operation

---
+++ app/concepts/exercise/contract/create.rb
@@ -0,0 +1,12 @@
+class Exercise::Contract::Create < Reform::Form
+  property :status
+  property :begining
+  property :ending
+  property :description
+  property :day_id
+  property :duration
+  # validates :begining, presence: true, timeliness: { type: :datetime }
+  # validates :ending, presence: true, timeliness: { type: :datetime }
+  # validates :status, length: {minimum: 4, maximum: 50}, presence: true
+  # validates :description, length: {minimum: 10, maximum: 2000}, presence: true
+end
--- app/concepts/exercise/operation/create.rb
+++ app/concepts/exercise/operation/create.rb
@@ -1,47 +1,36 @@
 class Exercise::Create < Trailblazer::Operation
-  step :process!
+  step Model( Exercise, :new )
+  step Contract::Build( constant: Exercise::Contract::Create )
+  step Contract::Validate()
+  step Contract::Persist()
+  step :create_exercise
+  step :calculate_duration
+  step :set_begining
+  step :set_ending
+  step :save_time
 
-  def process!(options, day)
-    day = Day.find(options['params'][:day_id])
-    day.exercises.create(options['params'].require(:exercise).permit(:status, :begining, :ending, :description, :created_at, :day_id, :id))
-    last_exercise = day.exercises.last
-    duration = TimeDifference.between(last_exercise.begining, last_exercise.ending).in_hours
-    time_creation_begining = "#{day.created_at.year}"+"-"+
-      "#{day.created_at.month}"+"-"+"#{day.created_at.day} #{options['params'][:exercise][:begining]}"
-    time_creation_ending = "#{day.created_at.year}"+"-"+
-      "#{day.created_at.month}"+"-"+"#{day.created_at.day} #{options['params'][:exercise][:ending]}"
-    last_exercise.update(duration: duration, begining: time_creation_begining,
-                         ending: time_creation_ending, created_at: time_creation_begining,
-                         updated_at: time_creation_ending)
+  def create_exercise(ctx, params:, **)
+    ctx[:exercise] = ctx[:day].exercises.create(params[:exercise])
+  end
+
+  def calculate_duration(ctx)
+    ctx[:duration] = TimeDifference.between(ctx[:exercise].begining, ctx[:exercise].ending).in_hours
+  end
+
+  def set_begining(ctx, params:, **)
+    ctx[:begining] = "#{ctx[:day].created_at.year}"+"-"+
+    "#{ctx[:day].created_at.month}"+"-"+"#{ctx[:day].created_at.day} #{params[:exercise][:begining]}"
+  end
+
+  def set_ending(ctx, params:, **)
+    ctx[:ending] = "#{ctx[:day].created_at.year}"+"-"+
+    "#{ctx[:day].created_at.month}"+"-"+"#{ctx[:day].created_at.day} #{params[:exercise][:ending]}"
+  end
+
+  def save_time(ctx)
+    ctx[:exercise].update(duration: ctx[:duration], begining: ctx[:begining],
+                    ending: ctx[:ending], created_at: ctx[:begining],
+                    updated_at: ctx[:ending])
   end
 end
 
-# class Exercise::Create < Trailblazer::Operation
-#   step :create_exercise
-#   step :calculate_duration
-#   step :set_begining
-#   step :set_ending
-#   step :save_time
-
-#   def save_time(duration)
-#     exercise.update(duration: duration, begining: time_creation_begining,
-#                     ending: time_creation_ending, created_at: time_creation_begining,
-#                     updated_at: time_creation_ending)
-#   end
-
-#   def set_ending(options, day)
-#     "#{day.created_at.year}"+"-"+"#{day.created_at.month}"+"-"+"#{day.created_at.day} #{options['params'][:exercise][:ending]}"
-#   end
-
-#   def set_begining(options, day)
-#     "#{day.created_at.year}"+"-"+"#{day.created_at.month}"+"-"+"#{day.created_at.day} #{options['params'][:exercise][:begining]}"
-#   end
-
-#   def calculate_duration(exercise)
-#     save_time TimeDifference.between(exercise.begining, exercise.ending).in_hours
-#   end
-
-#   def create_exercise(options, day)
-#     calculate_duration day.exercises.create(options['params'].require(:exercise).permit(:status, :begining, :ending, :description, :created_at, :day_id, :id))
-#   end
-# end
