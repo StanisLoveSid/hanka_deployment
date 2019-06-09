c0bc027605a8371d9f28e7263f2d523e7257ab7a
StanisLove Sid <joingamings@gmail.com>
2019-06-09 21:20:59 +0300

    refactor

--- app/concepts/glucometer_bsls/operation/create.rb
+++ app/concepts/glucometer_bsls/operation/create.rb
@@ -11,9 +11,6 @@ class GlucometerBsls::Operation::Create < Trailblazer::Operation
   step :create_bsl  
 
   def set_up_date(ctx, params:, **)
-    ctx[:found_year] = 0
-    ctx[:found_month] = 0
-    ctx[:found_day] = 0
     ctx[:date] = params[:sugar_level][:date].to_datetime
   end
 
@@ -21,34 +18,34 @@ class GlucometerBsls::Operation::Create < Trailblazer::Operation
     ctx[:user] = User.find_by(email: params[:sugar_level][:email])
   end
 
-  def find_or_create_year(ctx, **)
-    if ctx[:user].years.map(&:year_number).include? ctx[:date].year
-      user_years = ctx[:user].years.map {|year| { year.year_number => year.id } }
-      user_years.each { |year| ctx[:found_year] = Year.find year[ctx[:date].year] unless year[ctx[:date].year].nil? }
+  def find_or_create_year(ctx, date:, user:, **)
+    if user.years.map(&:year_number).include? date.year
+      ctx[:found_year] = Year.find user.years.map {|year| { year.year_number => year.id } }
+                                             .select { |year| year[date.year].present? }.first[date.year]
     else
-      ctx[:found_year] = Year.create(user_id: ctx[:user].id, created_at: ctx[:date], year_number: ctx[:date].year)
+      ctx[:found_year] = Year.create(user_id: user.id, created_at: date, year_number: date.year)
     end
   end
 
-  def find_or_create_month(ctx, params:, **)
-    if ctx[:found_year].months.map{|month| month.created_at.strftime("%Y-%m") }.include? params[:sugar_level][:date].to_date.strftime("%Y-%m")
-      all_months = ctx[:found_year].months.map { |month| { month.created_at.month => month.id } }
-      all_months.each { |month| ctx[:found_month] = Month.find month[ctx[:date].month]  unless month[ctx[:date].month].nil? }
+  def find_or_create_month(ctx, params:, found_year:, user:, date:, **)
+    if found_year.months.map{|month| month.created_at.strftime("%Y-%m") }.include? params[:sugar_level][:date].to_date.strftime("%Y-%m")
+      ctx[:found_month] = Month.find found_year.months.map { |month| { month.created_at.month => month.id } }
+                                                      .select { |month| month[date.month].present? }.first[date.month]
     else
-      ctx[:found_month] = Month.create(year_id: ctx[:found_year].id, user_id: ctx[:user].id, month_name: Date::MONTHNAMES[ctx[:date].month], created_at: ctx[:date])
+      ctx[:found_month] = Month.create(year_id: found_year.id, user_id: user.id, month_name: Date::MONTHNAMES[date.month], created_at: date)
     end
   end
 
-  def find_or_create_day(ctx, params:, **)
-    if ctx[:found_month].days.map(&:created_at).include? params[:sugar_level][:date].to_date
-      all_days =  ctx[:found_month].days.map { |day| { day.day_number => day.id } }
-      all_days.each { |day| ctx[:found_day] = Day.find day[ctx[:date].day]  unless day[ctx[:date].day].nil? }
+  def find_or_create_day(ctx, params:, found_month:, date:, user:, **)
+    if found_month.days.map(&:created_at).include? params[:sugar_level][:date].to_date
+      ctx[:found_day] = Day.find found_month.days.map { |day| { day.day_number => day.id } }
+                                                    .select { |day| day[date.day].present? }.first[date.day]
     else
-      ctx[:found_day] = Day.create(day_number: ctx[:date].day, created_at: ctx[:date], user_id: ctx[:user].id, month_id: ctx[:found_month].id)
+      ctx[:found_day] = Day.create(day_number: date.day, created_at: date, user_id: user.id, month_id: found_month.id)
     end
   end
 
-  def create_bsl(ctx, params:, **)
-    ctx[:found_day].sugar_levels.create(mmol: params[:sugar_level][:mmol], created_at: ctx[:date])
+  def create_bsl(ctx, params:, found_day:, date:, **)
+    found_day.sugar_levels.create(mmol: params[:sugar_level][:mmol], created_at: date)
   end
 end
